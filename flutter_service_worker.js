592c84ecf15cbb881459c3caf0e80dec70169218
Madhu Jayarama <madhu.gowda.j@gmail.com>
2020-08-16 10:37:26 -0400

    Added Business, Projects and Certificate card

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "ec44da294ee55a35fb58fe0c2b40cf57",
 "/": "ec44da294ee55a35fb58fe0c2b40cf57",
-"main.dart.js": "efe97179c6ed0c93e98214011413b007",
+"main.dart.js": "945b9b2c0355d90aaf9f5472e039bab7",
 "manifest.json": "4687e6b97134bd54ce60622ede735eb4"
 };
 
